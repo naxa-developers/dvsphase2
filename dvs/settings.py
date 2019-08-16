33563de9ec1a148b894ce601f5036a83f5bdbd67
Ubuntu <ubuntu@ip-172-26-13-245.us-east-2.compute.internal>
2019-08-16 06:14:36 +0000

    master

--- dvs/settings.py
+++ dvs/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         "PASSWORD":'',
-        'HOST': '172.31.0.2',
+        'HOST': '172.19.0.2',
         'PORT': '5432'
     }
 }
