aade2428cf1fbfdae22371cdc62acf3df5478237
sumit <rounnn8@gmail.com>
2019-08-16 11:36:39 +0545

    local_setting created

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 # python manage.py compilemessages -l ne -l en
 # python manage.py migrate --no-input
 pytest
