478910384e46bb6981f90c60559e94c7e47217ee
metalica20 <rounnn8@gmail.com>
2019-08-20 13:33:59 +0545

    Develop (#18)
    * Add README.md
    
    * readme ignore
    
    * readme
    
    * readme
    
    * readme
    
    * readme removed from cached
    
    * slack
    
    * federal models added
    
    * District ,Province,Gapanapa api added

--- testing/test/test_api.py
+++ testing/test/test_api.py
@@ -1,15 +1,9 @@
 from django.test import RequestFactory
 from django.urls import reverse
-from rest_framework.test import APIRequestFactory,APITestCase
+from rest_framework.test import APIRequestFactory, APITestCase
 
 
-
-class TestViews(APITestCase,APIRequestFactory):
-
-    def test_productapi(self):
-        url = reverse('Hydro')
-        response = self.client.get(url, format='json')
-        assert response.status_code == 200
+class TestViews(APITestCase, APIRequestFactory):
 
     def test_partnernapi(self):
         url = reverse('partner')
@@ -19,6 +13,30 @@ class TestViews(APITestCase,APIRequestFactory):
 
     def test_programapi(self):
         url = reverse('program')
+        response = self.client.get(url, format='json')
+        print(response)
+        assert response.status_code == 200
+
+    def test_markerapi(self):
+        url = reverse('marker')
+        response = self.client.get(url, format='json')
+        print(response)
+        assert response.status_code == 200
+
+    def test_districtapi(self):
+        url = reverse('district')
+        response = self.client.get(url, format='json')
+        print(response)
+        assert response.status_code == 200
+
+    def test_provinceapi(self):
+        url = reverse('province')
+        response = self.client.get(url, format='json')
+        print(response)
+        assert response.status_code == 200
+
+    def test_gapanapaapi(self):
+        url = reverse('gapanapa')
         response = self.client.get(url, format='json')
         print(response)
         assert response.status_code == 200
