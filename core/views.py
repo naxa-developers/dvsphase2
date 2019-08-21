6548a8dfbe9b41d0f03028802a724c740ad7966b
sumit <rounnn8@gmail.com>
2019-08-21 13:58:35 +0545

    hook^

--- core/views.py
+++ core/views.py
@@ -24,7 +24,7 @@ class PartnerView(views.APIView):
 
 class ProgramView(views.APIView):
     """
-    get: listsss deployssss offffaaaaadaaa programmmmmmisaaa
+    get: listsss deployssss offffaaaaadaaa progra
             - parameters: search(from program)
             - description: search should be of type string.
     """
