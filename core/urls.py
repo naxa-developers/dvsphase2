b0ae5a95866120828681165945b96e714c39602c
sumit <rounnn8@gmail.com>
2019-09-10 13:38:54 +0545

    url for program changed

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from core import views
 
 urlpatterns = [
     path('partner/', views.PartnerView.as_view(), name='partner'),
-    path('program/', views.ProgramView.as_view(), name='program'),
+    # path('program/', views.ProgramView.as_view(), name='program'),
     path('marker-category/', views.MarkerCategoryApi.as_view(), name='marker'),
     path('marker-value/', views.MarkerValueApi.as_view(), name='marker'),
     path('district/', views.DistrictApi.as_view(), name='district'),
@@ -15,6 +15,6 @@ urlpatterns = [
     path('indicator-value/', views.IndicatorData.as_view({'get': 'list'}), name='indicator-value'),
     path('sector/', views.SectorApi.as_view({'get': 'list'}), name='sector'),
     path('sub-sector/', views.SubsectorApi.as_view({'get': 'list'}), name='sub-sector'),
-    path('program-test/', views.ProgramTestApi.as_view({'get': 'list'}), name='program-test'),
+    path('program/', views.ProgramTestApi.as_view({'get': 'list'}), name='program'),
 
 ]
