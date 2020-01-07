00807e551f0c02bb459e7e407da750c8a410b349
sumit <rounnn8@gmail.com>
2020-01-07 22:51:14 +0545

    ajax added for dynamic municipality in fivew form

--- dashboard/static/dashboard/js/customform.js
+++ dashboard/static/dashboard/js/customform.js
@@ -124,7 +124,7 @@ $.ajax({
     success: function(result){
 
 
-
+    var district = result.results
     $('#id_district_id').html("");
     for(var i = 0 ; i<district.length;i++){
     var prov_div="<option value="+district[i].id+">"+district[i].name+"</option>"
@@ -132,7 +132,31 @@ $.ajax({
     }
 
     }});
-    });
+    }); // end
+
+//ajax request for mun
+$('#id_district_id').on('change',function(){
+dist_id = $(this).val()
+$.ajax({
+    url: baseUrl+'api/v1/core/gapanapa/?district_id='+dist_id,
+//    headers: {
+//        'Authorization': "Token 8933c5dd02de389ab5ee69c17a9af49f3d83b938",
+//    },
+    method: 'GET',
+    success: function(result){
+
+
+
+    $('#id_municipality_id').html("");
+    var mun = result.results
+    for(var i = 0 ; i<mun.length;i++){
+    var prov_div="<option value="+mun[i].id+">"+mun[i].name+"</option>"
+    $('#id_municipality_id').append(prov_div);
+    }
+
+    }});
+    }); // end
+
 
 }); // document end
 
