9ecba05770587a4fcceb23b5ff9c4bd8332054c5
SandipNup <sandipneupane65@gmail.com>
2020-04-20 20:29:40 +0545

    codacy issue fix

--- covid/views.py
+++ covid/views.py
@@ -1,6 +1,4 @@
-from rest_framework import viewsets, views
-from rest_framework.response import Response
-
+from rest_framework import viewsets
 from .models import Ttmp
 from .serializers import TtmpSerializer
 from django_filters.rest_framework import DjangoFilterBackend
