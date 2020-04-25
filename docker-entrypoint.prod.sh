6c6372def3a91deea96d29b573c1e29212d16a4e
sumit <rounnn8@gmail.com>
2020-04-25 19:33:44 +0545

    django , resframwor , swagger package upgarded, gdal installed, gunicorn for production used

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -1,6 +1,5 @@
-pip install -r requirements.txt
+#pip install -r requirements.txt
 python manage.py collectstatic --no-input
-# python manage.py compilemessages -l ne -l en
+python manage.py compilemessages -l ne -l en
 python manage.py migrate --no-input
-pytest
-python manage.py runserver 0.0.0.0:8000
+gunicorn dvs.wsgi:application --bind 0.0.0.0:8000
