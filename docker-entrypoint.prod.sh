9a909e015591c2d5f8c263006916b797fc623d87
metalica20 <rounnn8@gmail.com>
2020-07-13 17:50:46 +0545

    production conf changed (#69)

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -2,4 +2,4 @@
 python manage.py collectstatic --no-input
 #python manage.py compilemessages -l ne -l en
 python manage.py migrate --no-input
-gunicorn dvs.wsgi:application --bind 0.0.0.0:8000
+gunicorn dvs.wsgi:application --bind 0.0.0.0:8000 --workers 5
