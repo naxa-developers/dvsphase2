afc842d6624ad35ec329c6d5374e59fbe1426270
sijandh35 <sumoseason35@gmail.com>
2021-05-12 14:24:10 +0545

    status

--- core/filters.py
+++ core/filters.py
@@ -4,12 +4,16 @@ from django.db.models import Q
 
 
 def fivew(supplier, program, component, sector, sub_sector, markers, markers_value, count, stat):
-    stat = stat[0]
+    if stat[0][0].lower() == 'c':
+        stat = ['completed', 'Completed']
+    elif stat[0][0].lower() == 'o':
+        stat = ['ongoing', 'Ongoing']
+
     name_list = ['supplier', 'program', 'component', 'sector', 'sub_sector', 'markers', 'markers_value', 'stat']
     value_list = [supplier, program, component, sector, sub_sector, markers, markers_value, stat]
     key_list = ['supplier_id__in', 'program_id__in', 'component_id__code__in', 'program_id__sector__id__in',
                 'program_id__sub_sector__id__in',
-                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__iexact']
+                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__in']
     filter_dict = {}
     for index, x in enumerate(value_list):
         if x and name_list[index] not in count:
@@ -21,13 +25,16 @@ def fivew(supplier, program, component, sector, sub_sector, markers, markers_val
 
 
 def fivew_province(province, supplier, program, component, sector, sub_sector, markers, markers_value, count, stat):
-    stat = stat[0]
+    if stat[0][0].lower() == 'c':
+        stat = ['completed', 'Completed']
+    elif stat[0][0].lower() == 'o':
+        stat = ['ongoing', 'Ongoing']
     name_list = ['province', 'supplier', 'program', 'component', 'sector', 'sub_sector', 'markers', 'markers_value',
                  'stat']
     value_list = [province, supplier, program, component, sector, sub_sector, markers, markers_value, stat]
     key_list = ['province_id__in', 'supplier_id__in', 'program_id__in', 'component_id__code__in',
                 'program_id__sector__id__in', 'program_id__sub_sector__id__in',
-                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__iexact']
+                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__in']
     filter_dict = {}
     for index, x in enumerate(value_list):
         if x and name_list[index] not in count:
@@ -55,13 +62,16 @@ def sankey(province, supplier, program, component, sector, sub_sector, markers, 
 
 def fivew_municipality(municipality, supplier, program, component, sector, sub_sector, markers, markers_value, count,
                        stat):
-    stat = stat[0]
+    if stat[0][0].lower() == 'c':
+        stat = ['completed', 'Completed']
+    elif stat[0][0].lower() == 'o':
+        stat = ['ongoing', 'Ongoing']
     name_list = ['municipality', 'supplier', 'program', 'component', 'sector', 'sub_sector', 'markers', 'markers_value',
                  'stat']
     value_list = [municipality, supplier, program, component, sector, sub_sector, markers, markers_value, stat]
     key_list = ['municipality_id__in', 'supplier_id__in', 'program_id__in', 'component_id__code__in',
                 'program_id__sector__id__in', 'program_id__sub_sector__id__in',
-                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__iexact']
+                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__in']
     filter_dict = {}
     for index, x in enumerate(value_list):
         if x and name_list[index] not in count:
@@ -73,13 +83,16 @@ def fivew_municipality(municipality, supplier, program, component, sector, sub_s
 
 
 def fivew_district(district, supplier, program, component, sector, sub_sector, markers, markers_value, count, stat):
-    stat = stat[0]
+    if stat[0][0].lower() == 'c':
+        stat = ['completed', 'Completed']
+    elif stat[0][0].lower() == 'o':
+        stat = ['ongoing', 'Ongoing']
     name_list = ['district', 'supplier', 'program', 'component', 'sector', 'sub_sector', 'markers', 'markers_value',
                  'stat']
     value_list = [district, supplier, program, component, sector, sub_sector, markers, markers_value, stat]
     key_list = ['district_id__in', 'supplier_id__in', 'program_id__in', 'component_id__code__in',
                 'program_id__sector__id__in', 'program_id__sub_sector__id__in',
-                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__iexact']
+                'program_id__marker_category__id__in', 'program_id__marker_value__id__in', 'status__in']
     filter_dict = {}
     for index, x in enumerate(value_list):
         if x and name_list[index] not in count:
