92c01d94f3417ebbe6675e84b766d27caacecdd8
manippoudel <manippoudel@gmail.com>
2021-06-29 13:28:10 +0545

    fix: about us url  api added /

--- about_us/urls.py
+++ about_us/urls.py
@@ -4,6 +4,6 @@ from .viewsets import AboutUsListApiView, ContactUsListApiView
 
 urlpatterns = [
     path('', AboutUsListApiView.as_view()),
-    path('contact_us', ContactUsListApiView.as_view())
+    path('contact_us/', ContactUsListApiView.as_view())
 
 ]
