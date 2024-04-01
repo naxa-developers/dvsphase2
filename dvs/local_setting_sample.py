a1920240d575d189b6b5123f7fb334b22456ab25
kaditya97 <kaditya9711@gmail.com>
2024-04-01 11:04:50 +0545

    fix: initial setup with python3.9

--- dvs/local_setting_sample.py
+++ dvs/local_setting_sample.py
@@ -1,11 +1,30 @@
-# Sample For Local Setting chane it as your system requirement
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         "PASSWORD":'',
-#         'HOST': '172.22.0.2',
-#         'PORT': '5432'
-#     }
-# }
+from .settings import *
+from pymongo import MongoClient
+
+DEBUG = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'noreply.dfid@gmail.com'
+EMAIL_HOST_PASSWORD = 'ldymrhxnhiwjgcpf'
+EMAIL_PORT = 587
+
+LOGIN_URL = '/dashboard/login/'
+LOGIN_REDIRECT_URL = '/dashboard/main/'
+LOGOUT_REDIRECT_URL = '/dashboard/login/'
+SITE_URL = 'https://admin.dvs-nepal.org/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Mongo
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "root")
+MONGO_HOST = os.environ.get("MONGO_HOST", "mongo")
+MONGO_PORT = os.environ.get("MONGO_PORT", "27017")
+MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "dvs")
+
+client = MongoClient(
+    f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}')
+MONGO_DB = client["dvs"]
