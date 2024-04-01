fbcb5cb38da6419cca875a729350974c492d8656
kaditya97 <kaditya9711@gmail.com>
2024-04-01 11:09:59 +0545

    fix: uncovered passwords

--- dvs/local_setting_sample.py
+++ dvs/local_setting_sample.py
@@ -5,8 +5,8 @@ DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'noreply.dfid@gmail.com'
-EMAIL_HOST_PASSWORD = 'ldymrhxnhiwjgcpf'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 LOGIN_URL = '/dashboard/login/'
